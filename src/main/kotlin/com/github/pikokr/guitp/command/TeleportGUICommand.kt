a5a4d3a08be82d1480ec590f7eb18844ac0a4950
파랑이 <parangee9706@gmail.com>
2021-03-28 12:04:13 +0900

    feat: change item

--- src/main/kotlin/com/github/pikokr/guitp/command/TeleportGUICommand.kt
+++ src/main/kotlin/com/github/pikokr/guitp/command/TeleportGUICommand.kt
@@ -6,23 +6,29 @@ import com.github.pikokr.guitp.plugin.GUITeleportPlugin
 import net.kyori.adventure.text.Component
 import org.bukkit.Material
 import org.bukkit.NamespacedKey
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
 import org.bukkit.persistence.PersistentDataType
 
 object TeleportGUICommand {
+    private val item = ItemStack(Material.BOOK).apply {
+        itemMeta = itemMeta.apply {
+            persistentDataContainer.set(NamespacedKey(GUITeleportPlugin.instance, "type"), PersistentDataType.STRING, "TP_MENU")
+            displayName(Component.text("티피권"))
+            addEnchant(Enchantment.MENDING, 0, true)
+            addItemFlags(ItemFlag.HIDE_ENCHANTS)
+        }
+    }
+
     fun register(builder: KommandBuilder) {
         builder.apply {
             then("give") {
                 then("player" to player()) {
                     executes {
                         val p = it.parseArgument<Player>("player")
-                        p.inventory.addItem(ItemStack(Material.KNOWLEDGE_BOOK).apply {
-                            itemMeta = itemMeta.apply {
-                                persistentDataContainer.set(NamespacedKey(GUITeleportPlugin.instance, "type"), PersistentDataType.STRING, "TP_MENU")
-                                displayName(Component.text("티피권"))
-                            }
-                        })
+                        p.inventory.addItem(item.clone())
                     }
                 }
             }
